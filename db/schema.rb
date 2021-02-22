c04120ff5c1d774c7c9bde49e2df94feed728faf
Fashic <fashic@bk.ru>
2021-02-22 18:27:39 +0300

    Add change background user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_161024) do
+ActiveRecord::Schema.define(version: 2021_02_22_151011) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_161024) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.string "profile_color", default: "#005a55"
   end
 
   add_foreign_key "questions", "users"
