d322bc4a968df57cb6e11eb93a9a59b0df6c1ce2
Fashic <fashic@bk.ru>
2021-07-10 10:31:01 +0300

    Edit view hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,6 +1,7 @@
 class HashtagsController < ApplicationController
   def show
-    hashtag = Hashtag.with_questions.find_by!(text: params[:text])
-    @questions = hashtag.questions
+    @hashtag = Hashtag.with_questions.find_by!(text: params[:text])
+    @questions = @hashtag.questions
+    @hashtags = Hashtag.with_questions
   end
 end
